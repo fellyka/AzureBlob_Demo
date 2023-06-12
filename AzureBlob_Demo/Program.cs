a8462795304e8a8a5a837c82cf01ff0c99ae1d36
fellly <fellyka@gmail.com>
2023-06-12 21:47:42 +0200

    Add project files.

---
+++ AzureBlob_Demo/Program.cs
@@ -0,0 +1,19 @@
+﻿using Azure.Storage.Blobs;
+using System;
+
+namespace AzureBlob_Demo
+{
+    public static class Program
+    {
+        
+        private static string connectionString = "DefaultEndpointsProtocol=https;AccountName=stsollersaccount;AccountKey=ajQ5sGYUVY5cVjpANpHeWxWkvF8cU7cO5OLn3Fao4kxdhJ9PNLgEYW+2ghSwIJ6O/QeXr9YADFrq+ASt3FYNYw==;EndpointSuffix=core.windows.net";
+        private static string containerName = "vscontainer";
+        static void Main(string[] args)
+        {
+            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+            blobServiceClient.CreateBlobContainer(containerName);
+            Console.WriteLine("Container created !");
+            Console.ReadKey();
+        }
+    }
+}
