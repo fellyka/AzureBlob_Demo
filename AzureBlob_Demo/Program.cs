88c14a7a4f7b94e9ea4b336325c63a5d75797e81
fellly <fellyka@gmail.com>
2023-06-12 22:08:22 +0200

    Upload a file after creating a blob

--- AzureBlob_Demo/Program.cs
+++ AzureBlob_Demo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Azure.Storage.Blobs;
+
 using System;
 
 namespace AzureBlob_Demo
@@ -8,11 +9,19 @@ namespace AzureBlob_Demo
         
         private static string connectionString = "DefaultEndpointsProtocol=https;AccountName=stsollersaccount;AccountKey=ajQ5sGYUVY5cVjpANpHeWxWkvF8cU7cO5OLn3Fao4kxdhJ9PNLgEYW+2ghSwIJ6O/QeXr9YADFrq+ASt3FYNYw==;EndpointSuffix=core.windows.net";
         private static string containerName = "vscontainer";
+        private static string blobName = "createdfromvs";
+        private static string location = @"C:\Users\felly\OneDrive\Desktop\AZ-900-Certificate.pdf";
         static void Main(string[] args)
         {
             BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
-            blobServiceClient.CreateBlobContainer(containerName);
-            Console.WriteLine("Container created !");
+            /* blobServiceClient.CreateBlobContainer(containerName);*/
+
+            BlobContainerClient blobContainerClient = blobServiceClient.GetBlobContainerClient(containerName);
+            BlobClient blobClient = blobContainerClient.GetBlobClient(blobName);
+
+            blobClient.Upload(location);
+
+            Console.WriteLine("Your File has been uploaded to the container !");
             Console.ReadKey();
         }
     }
